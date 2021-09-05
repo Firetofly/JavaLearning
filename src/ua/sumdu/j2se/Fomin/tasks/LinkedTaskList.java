c7a1f65b35f5834d745454a07cca3bf98dcee5e3
Firefly <fominsawork@gmail.com>
2021-09-05 18:07:47 +0400

    Created LinkedTaskList class.

---
+++ src/ua/sumdu/j2se/Fomin/tasks/LinkedTaskList.java
@@ -0,0 +1,116 @@
+package ua.sumdu.j2se.Fomin.tasks;
+
+import java.util.LinkedList;
+
+public class LinkedTaskList {
+
+    private static class Node{
+        Task item;
+        Node next;
+        Node prev;
+
+        Node(Task element, Node next, Node prev){
+            this.item=element;
+            this.next=next;
+            this.prev=prev;
+        }
+    }
+
+    transient Node first;
+    transient Node last;
+    private int size;
+
+    public void add(Task task){
+        addLast(task);
+    }
+
+    public boolean remove(Task task){
+        if (task==null){
+            for(Node x=first;x!=null;x=x.next){
+                if(x.item==null){
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        else{
+            for(Node x =first; x!=null; x=x.next){
+                if(task.equals(x.item)){
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+    void linkLast(Task task){
+        Node l =last;
+        Node newNode=new Node(task,l,null);
+        last=newNode;
+        if (l == null)
+            first=newNode;
+        else
+            l.next=newNode;
+        size++;
+    }
+
+    void addLast(Task task){
+        linkLast(task);
+    }
+
+    Task unlink(Node x){
+        //assert x != null
+        Task element=x.item;
+        Node next=x.next;
+        Node prev=x.prev;
+
+        if(prev==null)first=next;
+        else{
+            prev.next=next;
+            x.prev=null;
+        }
+
+        if(next==null)last=prev;
+        else{
+            next.prev=prev;
+            x.next=null;
+        }
+
+        x.item=null;
+        size--;
+        return element;
+    }
+    public Task get(int index){
+        if(index>=0 && index<=size){
+            return node(index).item;
+        }
+        else
+            throw new IndexOutOfBoundsException("Index out of range.");
+        }
+
+    Node node(int index) {
+        if(index<(size>>1)){
+            Node x=first;
+            for (int i = 0; i <index ; i++) {
+                x=x.next;
+            }
+            return x;
+        }
+        else{
+            Node x=last;
+            for (int i = size-1; i >index ; i--){
+                x=x.prev;
+            }
+            return x;
+        }
+    }
+}
+
+
+
+
+
