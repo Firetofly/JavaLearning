a0512f6d6f1baa6719cd1eee413d4e66e553ae8e
Firefly <fominsawork@gmail.com>
2021-09-08 23:36:22 +0400

    The ArrayTaskList and LinkedTaskList inherits from AbstractTaskList. Class methods are overridden.

--- src/ua/sumdu/j2se/Fomin/tasks/ArrayTaskList.java
+++ src/ua/sumdu/j2se/Fomin/tasks/ArrayTaskList.java
@@ -1,12 +1,8 @@
 package ua.sumdu.j2se.Fomin.tasks;
 
 
-import java.util.Arrays;
-import java.util.Objects;
-
-public class ArrayTaskList {
+public class ArrayTaskList extends AbstractTaskList {
     private Task[] tasks;
-    private int size;
     private int index;
 
     private static final int DEFAULT_CAPACITY = 10;
@@ -15,81 +11,66 @@ public class ArrayTaskList {
         tasks = new Task[DEFAULT_CAPACITY];
     }
 
-    public ArrayTaskList(int capacity){
+    public ArrayTaskList(int capacity) {
         tasks = new Task[capacity];
     }
 
-    public Task getTask(int index) throws IndexOutOfBoundsException{
-        if (index>0 && index<=this.index){
-            return tasks[index];
-        }
-        else
-            throw new IndexOutOfBoundsException("Index out of range.");
 
-    }
-
-    public void setTasks(Task[] tasks){
+    public void setTasks(Task[] tasks) {
         this.tasks = tasks;
     }
 
-    public void add(Task task) throws IllegalArgumentException{
+    public void add(Task task) throws IllegalArgumentException {
         if (task != null) {
             if (index == tasks.length)
                 increaseCapacity();
             tasks[index] = task;
             index++;
-            size++;
-        }
-        else
+            increaseSize();
+
+        } else
             throw new IllegalArgumentException("Parameters of this method should not be NULL.");
     }
 
-    protected void increaseCapacity(){
-        Task[] newTasks = new Task[tasks.length + tasks.length>>1];
-        System.arraycopy(tasks,0,newTasks,0,index-1);
-        tasks=newTasks;
+    protected void increaseCapacity() {
+        Task[] newTasks = new Task[tasks.length + tasks.length >> 1];
+        System.arraycopy(tasks, 0, newTasks, 0, index - 1);
+        tasks = newTasks;
     }
 
-    boolean remove(Task task) throws IllegalArgumentException{
-        if(task != null){
-            int taskIndex=indexOf(task);
-            System.arraycopy(tasks,taskIndex+1,tasks,taskIndex,index-taskIndex);
-            size--;
+    @Override
+    public boolean remove(Task task) throws IllegalArgumentException {
+        if (task != null) {
+            int taskIndex = indexOf(task);
+            System.arraycopy(tasks, taskIndex + 1, tasks, taskIndex, index - taskIndex);
+            decreaseSize();
             this.index--;
             return true;
-        }
-        else
+        } else
             throw new IllegalArgumentException("Parameters of this method should not be NULL.");
     }
 
     //If array does not contain an element, the method return -1
     public int indexOf(Task task) throws IllegalArgumentException {
-        if(task != null) {
+        if (task != null) {
             int result = -1;
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size(); i++) {
                 if (tasks[i] == task) {
                     result = i;
                     break;
                 }
             }
             return result;
-        }
-        else
+        } else
             throw new IllegalArgumentException("Parameters of this method should not be NULL.");
     }
 
-    public int size(){
-        return size;
-    }
+    @Override
+    public Task getTask(int index) throws IndexOutOfBoundsException {
+        if (index >= 0 && index <= this.index) {
+            return tasks[index];
+        } else
+            throw new IndexOutOfBoundsException("Index out of range.");
 
-    public ArrayTaskList incoming(int from, int to) throws IllegalArgumentException{
-        if (from>0 && to>0) {
-            ArrayTaskList taskList = new ArrayTaskList();
-            taskList.setTasks(Arrays.stream(tasks).filter(Objects::nonNull).filter(task -> (task.getStartTime() >= from && task.getEndTime() <= to) || (task.getTime() >= from && task.getTime() <= to)).
-                    toArray(Task[]::new));
-            return taskList;
-        }
-        else
-            throw new IllegalArgumentException("Arguments of this method should not be negative.");
     }
 }
--- src/ua/sumdu/j2se/Fomin/tasks/LinkedTaskList.java
+++ src/ua/sumdu/j2se/Fomin/tasks/LinkedTaskList.java
@@ -2,40 +2,47 @@ package ua.sumdu.j2se.Fomin.tasks;
 
 import java.util.LinkedList;
 
-public class LinkedTaskList {
+public class LinkedTaskList extends AbstractTaskList {
 
     transient Node first;
     transient Node last;
-    private int size;
 
-    private static class Node{
+    private static class Node {
         Task item;
         Node next;
         Node prev;
 
-        Node(Task element, Node next, Node prev){
-            this.item=element;
-            this.next=next;
-            this.prev=prev;
+        Node(Task element, Node next, Node prev) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
         }
     }
 
-    public void add(Task task){
-        addLast(task);
+    @Override
+    public void add(Task task) {
+        Node l = last;
+        Node newNode = new Node(task, l, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        increaseSize();
     }
 
-    public boolean remove(Task task){
-        if (task==null){
-            for(Node x=first;x!=null;x=x.next){
-                if(x.item==null){
+    @Override
+    public boolean remove(Task task) {
+        if (task == null) {
+            for (Node x = first; x != null; x = x.next) {
+                if (x.item == null) {
                     unlink(x);
                     return true;
                 }
             }
-        }
-        else{
-            for(Node x =first; x!=null; x=x.next){
-                if(task.equals(x.item)){
+        } else {
+            for (Node x = first; x != null; x = x.next) {
+                if (task.equals(x.item)) {
                     unlink(x);
                     return true;
                 }
@@ -44,89 +51,67 @@ public class LinkedTaskList {
         return false;
     }
 
-    public int size(){
-        return size;
+    public Task unlink(Node x) {
+        if (x != null) {
+            //assert x != null
+            Task element = x.item;
+            Node next = x.next;
+            Node prev = x.prev;
+
+            if (prev == null) first = next;
+            else {
+                prev.next = next;
+                x.prev = null;
+            }
+
+            if (next == null) last = prev;
+            else {
+                next.prev = prev;
+                x.next = null;
+            }
+
+            x.item = null;
+            decreaseSize();
+            return element;
+        } else throw new IllegalArgumentException("Parameter of this method should not be null.");
     }
-    void linkLast(Task task){
-        Node l =last;
-        Node newNode=new Node(task,l,null);
-        last=newNode;
-        if (l == null)
-            first=newNode;
-        else
-            l.next=newNode;
-        size++;
-    }
 
-    void addLast(Task task){
-        linkLast(task);
-    }
-
-    Task unlink(Node x){
-        //assert x != null
-        Task element=x.item;
-        Node next=x.next;
-        Node prev=x.prev;
-
-        if(prev==null)first=next;
-        else{
-            prev.next=next;
-            x.prev=null;
-        }
-
-        if(next==null)last=prev;
-        else{
-            next.prev=prev;
-            x.next=null;
-        }
-
-        x.item=null;
-        size--;
-        return element;
-    }
-    public Task get(int index) throws IndexOutOfBoundsException{
-        if(index>=0 && index<=size){
+    @Override
+    public Task getTask(int index) throws IndexOutOfBoundsException {
+        if (index >= 0 && index <= size()) {
             return node(index).item;
-        }
-        else
+        } else
             throw new IndexOutOfBoundsException("Index out of range.");
-        }
+    }
+
     //set with index
-    public Task set(Task task, int index){
-        if(index>=0 && index<size){
+    public Task set(Task task, int index) {
+        if (index >= 0 && index < size()) {
             Node x = node(index);
             Task oldVal = x.item;
-            x.item= task;
+            x.item = task;
             return oldVal;
-        }
-        else
+        } else
             throw new IndexOutOfBoundsException("Index out of range.");
-
-
     }
 
     Node node(int index) {
-        if(index<(size>>1)){
-            Node x=first;
-            for (int i = 0; i <index ; i++) {
-                x=x.next;
+        if (index >= 0 && index <= size()) {
+            if (index < (size() >> 1)) {
+                Node x = first;
+                for (int i = 0; i < index; i++) {
+                    x = x.next;
+                }
+                return x;
+            } else {
+                Node x = last;
+                for (int i = size() - 1; i > index; i--) {
+                    x = x.prev;
+                }
+                return x;
             }
-            return x;
-        }
-        else{
-            Node x=last;
-            for (int i = size-1; i >index ; i--){
-                x=x.prev;
-            }
-            return x;
-        }
+        } else throw new IndexOutOfBoundsException("Index out of range.");
     }
-
-    /*public LinkedTaskList incoming(int from, int to){
-        LinkedTaskList linkedTaskList = new LinkedTaskList();
-        linkedTaskList.
-
-    }*/
 }
 
 
