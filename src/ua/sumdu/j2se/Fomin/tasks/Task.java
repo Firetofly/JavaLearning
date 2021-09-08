5ae362ce3ec34fd7512f087bed2bbaced510f6ba
Firefly <fominsawork@gmail.com>
2021-09-08 23:37:01 +0400

    Just updated

--- src/ua/sumdu/j2se/Fomin/tasks/Task.java
+++ src/ua/sumdu/j2se/Fomin/tasks/Task.java
@@ -9,87 +9,85 @@ public class Task {
     private boolean active;
 
     //Constructor constructs an inactive task to run at a specified time without repeating with a given name.
-    public Task(String title, int time) throws IllegalArgumentException{
-        if (time>=0) {
+    public Task(String title, int time) throws IllegalArgumentException {
+        if (time >= 0) {
             this.title = title;
             this.time = time;
-        }
-        else
+        } else
             throw new IllegalArgumentException("Time should not be negative.");
     }
+
     /*
      * Constructor constructs an inactive task to run within the specified time range
      * (including the start and the end time) with the set repetition interval and with a given name.
      */
-    public Task (String title, int start, int end, int interval) throws IllegalArgumentException{
-        if (start>=0 && end>=0 && interval>=0){
-            this.title=title;
-            this.start=start;
-            this.end=end;
-            this.interval=interval;
-        }
-        else
+    public Task(String title, int start, int end, int interval) throws IllegalArgumentException {
+        if (start >= 0 && end >= 0 && interval >= 0) {
+            this.title = title;
+            this.start = start;
+            this.end = end;
+            this.interval = interval;
+        } else
             throw new IllegalArgumentException("StartTime,EndTime and Interval should not be negative.");
 
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title){
-        this.title=title;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public boolean isActive(){
+    public boolean isActive() {
         return active;
     }
 
-    public void setActive(boolean active){
-        this.active=active;
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     //Methods for reading and changing execution time for non-repetitive tasks
-    public int getTime(){
-        return isRepeated()?start:time;
+    public int getTime() {
+        return isRepeated() ? start : time;
     }
 
-    public void setTime(int time) throws IllegalArgumentException{
-        if(time>=0) {
+    public void setTime(int time) throws IllegalArgumentException {
+        if (time >= 0) {
             this.time = time;
             this.interval = 0;
             this.start = 0;
             this.end = 0;
-        }
-        else
+        } else
             throw new IllegalArgumentException("Time should not be negative.");
     }
 
     //Methods for reading and changing execution time for repetitive tasks
-    public int getStartTime(){
-        return isRepeated()?start:time;
+    public int getStartTime() {
+        return isRepeated() ? start : time;
     }
 
-    public int getEndTime(){
-        return isRepeated()?end:time;
+    public int getEndTime() {
+        return isRepeated() ? end : time;
     }
 
-    public int getRepeatInterval(){
+    public int getRepeatInterval() {
         return interval;
     }
+
     //If the task is a non-repetitive one, it should become repetitive.
-    public void setTime(int start, int end, int interval) throws IllegalArgumentException{
-        if(start>=0 && end>=0 && interval>=0){
-                this.start=start;
-                this.end=end;
-                this.interval=interval;
-                this.time=0;
-        }
-        else
+    public void setTime(int start, int end, int interval) throws IllegalArgumentException {
+        if (start >= 0 && end >= 0 && interval >= 0) {
+            this.start = start;
+            this.end = end;
+            this.interval = interval;
+            this.time = 0;
+        } else
             throw new IllegalArgumentException("StartTime,EndTime and Interval should not be negative.");
     }
 
-    public boolean isRepeated(){
+    public boolean isRepeated() {
         return getRepeatInterval() != 0;
     }
 
@@ -97,10 +95,9 @@ public class Task {
      *Method that returns the next start time of the task execution after the current time.
      *If after the specified time the task is not executed anymore, the method must return -1.
      */
-    public int nextTimeAfter(int current) throws IllegalArgumentException{
-
-        if (current>=0) {
-            if (isActive()) {
+    public int nextTimeAfter(int current) throws IllegalArgumentException {
+        if (isActive()) {
+            if (current >= 0) {
                 //For repetitive tasks
                 if (isRepeated()) {
                     if (current > start && (current <= (end - start) / interval * interval + start)) {
@@ -118,8 +115,7 @@ public class Task {
                 else return current <= time ? time : -1;
             } else
                 throw new IllegalArgumentException("Current should not be negative.");
-        }
-        else
+        } else
             return -1;
     }
 }
