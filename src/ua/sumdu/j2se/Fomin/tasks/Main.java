a6e1450a04cf7db169cfc58672af5e0d23792a5f
Firefly <fominsawork@gmail.com>
2021-09-08 23:36:45 +0400

    Added test data.

--- src/ua/sumdu/j2se/Fomin/tasks/Main.java
+++ src/ua/sumdu/j2se/Fomin/tasks/Main.java
@@ -2,23 +2,23 @@ package ua.sumdu.j2se.Fomin.tasks;
 
 public class Main {
     public static void main(String[] args) {
-        //Test for task 1/*
-       try {
-           Task task = new Task("task",100,-1,24);
-       }
-       catch (IllegalArgumentException e){
-           System.out.println(e.getMessage());
-       }
-       /*System.out.println("current: " + 90 + ", next: " + task.nextTimeAfter(90));
-        System.out.println("current: " + 100 + ", next: " + task.nextTimeAfter(100));
-        System.out.println("current: " + 110 + ", next: " + task.nextTimeAfter(110));
-        System.out.println("current: " + 120 + ", next: " + task.nextTimeAfter(120));
-        System.out.println("current: " + 124 + ", next: " + task.nextTimeAfter(124));
-        System.out.println("current: " + 130 + ", next: " + task.nextTimeAfter(130));
-        System.out.println("current: " + 140 + ", next: " + task.nextTimeAfter(140));
-        System.out.println("current: " + 148 + ", next: " + task.nextTimeAfter(148));
-        System.out.println("current: " + 149 + ", next: " + task.nextTimeAfter(149));
-        System.out.println("current: " + 150 + ", next: " + task.nextTimeAfter(150));*/
+        //Test for task 1
+//        try {
+//            Task task = new Task("task1", 100, 150, 24);
+//            task.setActive(true);
+//            System.out.println("current: " + 90 + ", next: " + task.nextTimeAfter(90));
+//            System.out.println("current: " + 100 + ", next: " + task.nextTimeAfter(100));
+//            System.out.println("current: " + 110 + ", next: " + task.nextTimeAfter(110));
+//            System.out.println("current: " + 120 + ", next: " + task.nextTimeAfter(120));
+//            System.out.println("current: " + 124 + ", next: " + task.nextTimeAfter(124));
+//            System.out.println("current: " + 130 + ", next: " + task.nextTimeAfter(130));
+//            System.out.println("current: " + 140 + ", next: " + task.nextTimeAfter(140));
+//            System.out.println("current: " + 148 + ", next: " + task.nextTimeAfter(148));
+//            System.out.println("current: " + 149 + ", next: " + task.nextTimeAfter(149));
+//            System.out.println("current: " + 150 + ", next: " + task.nextTimeAfter(150));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        }
 
         //Test for task 2
         ArrayTaskList tasksList= new ArrayTaskList();
@@ -33,6 +33,14 @@ public class Main {
             System.out.println("Exception message: "+o.getMessage());
             System.out.println("Cause: "+o.getCause());
         }
+
+
+        for (int i = 0; i <tasksList.size() ; i++) {
+            System.out.println("Start time: "+tasksList.getTask(i).getStartTime());
+            System.out.println("End time: "+tasksList.getTask(i).getEndTime());
+            System.out.println("Title: "+tasksList.getTask(i).getTitle()+"\n");
+        }
+
         tasksList.remove(task1);
         tasksList.remove(task2);
         System.out.println(tasksList.size());
